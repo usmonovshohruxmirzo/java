859a517ecd8138196da16b281a247f7f14f64efc
Shohruxmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2024-09-06 10:35:55 +0500

    feat: DataTypes

--- main/src/main/DataTypes.java
+++ main/src/main/DataTypes.java
@@ -28,10 +28,52 @@ public class DataTypes {
 	long l = 50000L;
 	double d = 12.32;
 	
-	System.out.println(number);
+//	System.out.println(number);
+//	System.out.println(bool);
+//	System.out.println(text);
+//	System.out.println(floatNumber);
+//	System.out.println(floatNumber2);
+//	System.out.println(letter);
+//	System.out.println(b);
+//	System.out.println(s);
+//	System.out.println(l);
+//	System.out.println(d);
+	
 	
 	// Integer types: byte, short, int, long
 	// Float: float, double
+	
+	// byte - The byte data type can store whole numbers from -128 to 127. This can be used instead of int or other integer types to save memory when you are certain that the value will be within -128 and 127:
+	byte myNum = 10;
+	System.out.println(myNum);
+	
+	// short - data type can store whole numbers from -32768 to 32767:
+	short myShort = 32767;
+	System.out.println(myShort);
+	
+	// int - The int data type can store whole numbers from -2147483648 to 2147483647. In general, and in our tutorial, the int data type is the preferred data type when we create variables with a numeric value.
+	int myInt = 2147483647;
+	System.out.println(myInt);
+	
+	// long - The long data type can store whole numbers from -9223372036854775808 to 9223372036854775807. This is used when int is not large enough to store the value. Note that you should end the value with an "L":
+	long myLong = 15000000000L;
+	System.out.println(myLong);
+	
+	// Floating Point Types - The float and double data types can store fractional numbers. Note that you should end the value with an "f" for floats and "d" for doubles:
+	float myF = 5.75f;
+	System.out.println(myF);
+	
+	double myD = 19.99d;
+	System.out.println(myD);
+	 /*
+	 * Use float or double?
+	 * The precision of a floating point value indicates how many digits the value can have after the decimal point. The precision of float is only six or seven decimal digits, while double variables have a precision of about 15 digits. Therefore it is safer to use double for most calculations.
+	 */
+	
+	float f1 = 35e3f;
+	double d1 = 12E4d;
+	System.out.println(f1);
+	System.out.println(d1);
 	
 	}	
 }

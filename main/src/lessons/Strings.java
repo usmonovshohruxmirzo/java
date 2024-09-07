888f603399bb0d2279f28ab328d6c57e09366269
Shohruxmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2024-09-07 14:42:33 +0500

    feat(projects): Calculator

---
+++ main/src/lessons/Strings.java
@@ -0,0 +1,7 @@
+package lessons;
+
+public class Strings {
+	public static void main(String[] args) {
+		
+	}
+}
--- main/src/projects/Calculator.java
+++ main/src/projects/Calculator.java
@@ -1,5 +1,39 @@
 package projects;
 
 public class Calculator {
-
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 2;
+		int result;
+		String operator = "+";
+		
+		switch(operator) {
+		case "+":
+			result = a + b;
+			System.out.print(a + " + " + b + " = ");
+			System.out.print(result);
+		break;
+		case "-":
+			result = a - b;
+			System.out.print(a + " - " + b + " = ");
+			System.out.print(result);
+		break;
+		case "/":
+			if (b == 0) {
+				System.err.println(a + " Cannot divided by " + b);
+			} else {
+				result = a - b;
+				System.out.print(a + " / " + b + " = ");
+				System.out.print(result);
+			}
+		break;
+		case "*":
+			result = a - b;
+			System.out.print(a + " * " + b + " = ");
+			System.out.print(result);
+		break;
+		default: System.out.println("operator not found");
+		}
+		
+	}
 }

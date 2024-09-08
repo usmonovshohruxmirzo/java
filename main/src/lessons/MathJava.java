2866035a88acba49dcef8ef0112512996266d390
Shohruxmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2024-09-08 21:25:38 +0500

    Math

---
+++ main/src/lessons/MathJava.java
@@ -0,0 +1,12 @@
+package lessons;
+
+public class MathJava {
+	public static void main(String[] args) {
+		System.out.println(Math.max(5, 10));
+		System.out.println(Math.min(5, 10));
+		System.out.println((int) Math.sqrt(64));
+		System.out.println(Math.abs(-10));
+		System.out.println(Math.random());
+		System.out.println((int) (Math.random() * 100));
+	}
+}
--- main/src/lessons/Strings.java
+++ main/src/lessons/Strings.java
@@ -2,6 +2,30 @@ package lessons;
 
 public class Strings {
 	public static void main(String[] args) {
+		String text = "Hello World";
 		
+		System.out.println(text);
+		System.out.println(text.length());
+		System.out.println(text.toUpperCase());
+		System.out.println(text.toLowerCase());
+		System.out.println(text.indexOf("H"));
+		
+		// String Concatenation
+		
+		String firstName = "Usmonov";
+		String lastName = "Shohruhmirzo";
+		
+		System.out.println(firstName + " " + lastName);
+		System.out.println(firstName.concat(lastName));
+		
+		int x = 10;
+		String y = "20";
+		System.out.println(x + y);
+		
+		// Special Characters
+		System.out.println("Hello\tWorld"); // \t
+		System.out.println("Hello\nWorld"); // \n
+		System.out.println("Hello\rWorld"); // \r
+		System.out.println("Hel\blo World"); // \b
 	}
 }
